3eee41430a4ab85724c7ee80566256f184cdb2b2
Jan Papoušek <jan.papousek@gmail.com>
2013-11-07 12:06:32 +0100

    improved simulation framework

--- libs/geodata/simulation.py
+++ libs/geodata/simulation.py
@@ -1,15 +1,30 @@
 import math
 import numpy as np
+from pprint import pprint
+import pandas as pd
+
 
 class Simulator:
 
-    def __init__(self, answers):
-        self.answers = answers
+    def __init__(self, answers, sorted = False):
+        if not sorted:
+            self.answers = answers.sort(['inserted'])
+        else:
+            self.answers = answers
+
+    def __getitem__(self, key):
+        if np.isscalar(key):
+            return self.answers[key]
+        else:
+            return Simulator(self.answers[key], True)
+
+    def step_by_step(self, model):
+        return StepByStepSimulation(model, self.answers)
 
     def simulate(self, model, status = False, n = None):
         predicted = []
         answered = []
-        data = self.answers.sort(["inserted"]).data
+        data = self.answers.data
         progress_before = None
         index = 0
         if n == None or n > len(data):
@@ -30,6 +45,37 @@ class Simulator:
         return SimulationResult(model, predicted, answered)
 
 
+class StepByStepSimulation:
+
+    def __init__(self, model, answers):
+        self._step = 0
+        self.answers = answers
+        self.answers = self.answers.sort(["inserted"])
+        self.answers.data.index = range(len(self.answers))
+        self.model = model
+        self.predicted = []
+        self.answered = []
+        self.last_answer = None
+
+    def step(self):
+        if self._step >= len(self.answers):
+            return None
+        row = self.answers.data.xs(self._step)
+        self.predicted.append(self.model.predict(row.to_dict()))
+        self.answered.append(row['place.asked'] == row['place.answered'])
+        self.model.save(row.to_dict())
+        self._step += 1
+        self.last_answer = row
+        return self
+
+    def rmse(self):
+        return math.sqrt(
+            sum((self.predicted - self.answered) ** 2)
+            /
+            float(len(self.predicted))
+        )
+
+
 class Model:
 
     def save(self, answer):
@@ -40,6 +86,18 @@ class Model:
 
     def difficulties(self, places):
         raise Exception("The difficulties(places) method is not defined")
+
+    def predictions(self, places, user):
+        result = []
+        for i, place in places.data.iterrows():
+            question = {
+                'user': user,
+                'place.asked': place['id'],
+                'type': 10}
+            result.append({
+                'code': place['code'],
+                'ratio': self.predict(question)})
+        return pd.DataFrame(result)
 
     def sigmoid(self, x):
         return 1 / (1 + math.exp(-x))
