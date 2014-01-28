e5ff140eef421363d33ecbfb28867efd7d7b3534
Jan Papoušek <jan.papousek@gmail.com>
2014-01-28 11:15:55 +0100

    better naming

--- libs/proso/geodata/plot.py
+++ libs/proso/geodata/plot.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def confusing_factor_by_distance(cf, distance_matrix, places=None, **args):
+def confusing_factor_by_distance(cf, distance_matrix, places=None, **kwargs):
     def sort_key_gen(place):
         return lambda (p, v): (distance_matrix[place][p], -v)
 
-    confusing_factor(cf, places, sort_key_gen, **args)
+    confusing_factor(cf, places, sort_key_gen, **kwargs)
 
 
-def confusing_factor(confusing_factor, places=None, sort_key_gen=None, **args):
+def confusing_factor(confusing_factor, places=None, sort_key_gen=None, **kwargs):
     if not places:
         places = confusing_factor.keys()
     normalized = {}
@@ -24,17 +24,17 @@ def confusing_factor(confusing_factor, places=None, sort_key_gen=None, **args):
             lambda x: x[1] / total,
             sorted(confusing_factor[place].iteritems(), key=sort_key))
     for place in places:
-        plt.plot(range(len(normalized[place])), normalized[place], **args)
+        plt.plot(range(len(normalized[place])), normalized[place], **kwargs)
 
 
-def answers_per_user(answers, **args):
+def answers_per_user(answers, **kwargs):
     per_user = {}
     for i, row in answers.iterrows():
         per_user[row['user']] = per_user.get(row['user'], 0) + 1
-    plt.hist(per_user.values(), **args)
+    plt.hist(per_user.values(), **kwargs)
 
 
-def total_success(answers, history_length, **args):
+def total_success(answers, history_length, **kwargs):
     x = []
     y = []
     corrects = []
@@ -50,10 +50,10 @@ def total_success(answers, history_length, **args):
         count += 1
         x.append(row['inserted'])
         y.append(float(total_sum) / min(history_length, count))
-    plt.plot(x, y, **args)
+    plt.plot(x, y, **kwargs)
 
 
-def total_answers(answers, **args):
+def total_answers(answers, **kwargs):
     x = []
     y = []
     count = 1
@@ -61,4 +61,4 @@ def total_answers(answers, **args):
         x.append(row['inserted'])
         y.append(count)
         count += 1
-    plt.plot(x, y, **args)
+    plt.plot(x, y, **kwargs)
