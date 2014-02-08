5dc7d07f8c2c8d129419bae75d229bdc6b16d104
Jan Papoušek <jan.papousek@gmail.com>
2014-02-08 19:16:25 +0100

    weighted binary RMSE

--- libs/proso/metrics.py
+++ libs/proso/metrics.py
@@ -10,6 +10,14 @@ def rmse(expected, found):
     util.ensure.same_lengths(expected, found)
     return math.sqrt(np.mean((np.array(expected) - np.array(found)) ** 2))
 
+def rmse_binary_weighted(expected, found):
+    util.ensure.same_lengths(expected, found)
+    ones = sum(expected) / float(len(expected))
+    zeros = 1 - ones
+    return math.sqrt(np.mean(np.array(map(
+        lambda x: zeros * x if x > 0 else ones * x,
+        np.array(expected) - np.array(found))) ** 2))
+
 
 def logloss(expected, found):
     util.ensure.same_lengths(expected, found)
